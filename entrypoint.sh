e44e862317ba7399b943ab76dbc115680623e73e
DamCo <73948784+Damdam86@users.noreply.github.com>
2025-07-03 16:41:08 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "🚀 Starting Odoo with custom Supabase configuration..."
 exec odoo \
   --db_host=db.dkdrcflwmdkngitketzm.supabase.co \
   --db_port=5432 \
-  --db_user=postgres \
-  --db_password=Damdam86!? \
+  --db_user=odoo_user \
+  --db_password=OdooSafe123! \
   -i base \
   --without-demo=all
