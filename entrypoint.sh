6850ea5cdab522d3c501ffb2279af8231429442a
DamCo <73948784+Damdam86@users.noreply.github.com>
2025-07-04 16:59:09 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # Première initialisation si la base est vide
 if [ ! -f "/etc/odoo/.initialized" ]; then
     echo "🔧 Initialisation de Odoo avec tous les modules..."
-    /usr/bin/odoo -i all --without-demo=all --save
+    /usr/bin/odoo -i all --without-demo=all
     touch /etc/odoo/.initialized
 else
     echo "🚀 Lancement standard de Odoo"
