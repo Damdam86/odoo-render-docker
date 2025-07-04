0cca0fc93b6a5014e322a336ec6ea8c792002508
DamCo <73948784+Damdam86@users.noreply.github.com>
2025-07-04 14:56:54 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,13 @@ echo "PGUSER=$PGUSER"
 echo "PGPASSWORD=$PGPASSWORD"
 echo "PGDATABASE=$PGDATABASE"
 
-# Vérifier que toutes les variables sont là
-if [ -z "$PGHOST" ] || [ -z "$PGPORT" ] || [ -z "$PGUSER" ] || [ -z "$PGPASSWORD" ] || [ -z "$PGDATABASE" ]; then
+# Vérification
+if [ -z "$PGHOST" ] || [ -z "$PGPORT" ] || [ -z "$PGUSER" ] || [ -z "$PGPASSWORD" ]; then
   echo "❌ Missing database environment variables!"
   exit 1
 fi
 
-# Forcer SSL (mais sans variables vides)
+# Forcer SSL
 export PGSSLMODE=require
 
 exec odoo \
@@ -22,7 +22,6 @@ exec odoo \
   --db_port="$PGPORT" \
   --db_user="$PGUSER" \
   --db_password="$PGPASSWORD" \
-  --db_name="$PGDATABASE" \
   --log-level=debug \
   --http-port=8069 \
   --http-interface=0.0.0.0
